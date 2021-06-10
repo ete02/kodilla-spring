9f405484b452164af6b39033f390976294bbdc0b
Ievgen Teslenko <eugen.te11@gmail.com>
2021-06-10 23:16:55 +0200

    Create new variables for mail template: Kodilla task 32.2

---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class CompanyConfig {
+    @Value("${info.app.company.name}")
+    private String companyName;
+
+    @Value("${info.app.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.app.company.email}")
+    private String companyMail;
+
+    @Value("${info.app.company.phone}")
+    private String companyPhone;
+}
